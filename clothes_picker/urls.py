fd38813be8e4b192ef3de98b62b76d8e66cc52b6
Dm1tryD <dmitrydanyliukk@gmail.com>
2021-01-13 15:20:34 +0200

    Added templates and test view

--- clothes_picker/urls.py
+++ clothes_picker/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('generate/', include('generate_clothes.urls')),
+    path('', include('generate_clothes.urls')),
     path('admin/', admin.site.urls),
 ]
